ad7bdde4f7d43386f2aa4911301fa44be77527b2
phenom_saswat_mohanty <saswat.mohanty@phenompeople.com>
2020-06-19 00:28:49 +0530

    login page layout added

--- src/pages/flows/flows.jsx
+++ src/pages/flows/flows.jsx
@@ -2,5 +2,4 @@ import React from 'react';
 
 const Flows = () => <div>Flows</div>;
 
-
 export default Flows;
--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,5 +1,20 @@
 import React from 'react';
 
-const Login = () => <div>Login</div>
+const Login = () => {
+    return <div className="login">
+        <div className="login-section">
+            <div>
+                Login
+            </div>
+            <div className="form-group">
+              <input type="text" className="form-control" placeholder="Email" />
+            </div>
+            <div className="form-group">
+              <input type="password" className="form-control" placeholder="Password" />
+            </div>
+            <button className="btn btn-login">Login</button>
+        </div>
+    </div>;
+}
 
 export default Login;
