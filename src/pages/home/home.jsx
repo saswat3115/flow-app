c4bd1306e7046b056b28b112680bfa8c5deacff8
phenom_saswat_mohanty <saswat.mohanty@phenompeople.com>
2020-06-19 00:44:15 +0530

    home page layout added

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
 
-const Home = () => <div>Home</div>;
+const Home = () => {
+    return <div className="home">
+        <div className="row search-section">
+            <div className="col">
+                <input type="text" placeholder="Search Workflow" className="input-search" />
+                <span className="btn-filter">
+                    Filter
+                </span>
+            </div>
+            <div className="col">
+                <button>Create Workflow</button>
+            </div>
+        </div>
+        <div className="flows-container">
+            {/* TODO: List of Flows */}
+        </div>
+    </div>;
+}
 
 export default Home;
